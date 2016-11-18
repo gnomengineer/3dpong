5d9733d6a397b1d6f32cea3945fcf1e083b97411
troopa <troopa.der.pate@gmail.com>
2016-11-18 08:12:22 +0100

    updated gl-helper.js

--- gl-helper.js
+++ gl-helper.js
@@ -24,7 +24,7 @@ function setUpUniforms( uniformNames ) {
 
 function setUpAttributes (attributeNames ){
     attributeNames.forEach(function (entry) {
-            shaderProgram.attributes[entry] = gl.getAttribLocation(shaderProgram, entry)
+            shaderProgram.glAttributes[entry] = gl.getAttribLocation(shaderProgram, entry)
         }
     )
 }
@@ -55,32 +55,51 @@ function drawStrip(bufferPos,bufferColor, numberOfVectors){
     gl.drawArrays(gl.TRIANGLE_STRIP,0,4);
 }
 
-function drawTriangles(edgeBuffer, vertexBuffer) {
-    enableBuffer(vertexBuffer,3);
+/**
+ *
+ * @param attributeName
+ * @param edgeBuffer
+ * @param vertexBuffer
+ */
+function drawTriangles(attributeName, edgeBuffer, vertexBuffer) {
+    enableBuffer(shaderProgram.glAttributes[attributeName], vertexBuffer,3);
     gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, edgeBuffer);
     gl.drawElements(gl.TRIANGLES, edgeBuffer.length, gl.UNSIGNED_SHORT, 0);
 }
 
-function drawLine(bufferPos){
-    enablePositionBuffer(bufferPos, 2);
+/**
+ * draws a line between the points in the given buffer.
+ *
+ * @param attributeName
+ * @param bufferPos
+ */
+function drawLine(attributeName, bufferPos){
+    enableBuffer(shaderProgram.glAttributes[attributeName], bufferPos, 2);
     gl.lineWidth = .5;
     gl.drawArrays(gl.LINES,0,2)
-}jf
-
-function drawCircle(bufferPosition){
-    enablePositionBuffer(bufferPosition, 4);
-
-    gl.drawArrays(gl.LINE_LOOP,0,16);
 }
 
+/**
+ * draws a circle with the given points in the buffer.
+ *
+ * @param attributeName - name of the target gl attribute
+ * @param bufferPosition - buffer with positions in it
+ * @param verticesCount - number of positions
+ */
+function drawCircle(attributeName, bufferPosition, verticesCount){
+    enableBuffer(shaderProgram.glAttributes[attributeName], bufferPosition, 4);
+    gl.drawArrays(gl.LINE_LOOP,0,verticesCount);
+}
+
+/**
+ * enables the given buffer onto the given attribute.
+ *
+ * @param attributeID - target attributeID
+ * @param buffer - gl buffer
+ * @param n - amount of vertices
+ */
 function enableBuffer(attributeID, buffer, n){
     gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
     gl.vertexAttribPointer( attributeID, n , gl.FLOAT, false,0,0);
     gl.enableVertexAttribArray( attributeID );
-}
-
-function enableColorBuffer(bufferColor) {
-    gl.bindBuffer(gl.ARRAY_BUFFER, bufferColor);
-    gl.vertexAttribPointer( shaderProgram.aColorPositionId, 4, gl.FLOAT, false, 0,0);
-    gl.enableVertexAttribArray(shaderProgram.aColorPositionId);
 }
